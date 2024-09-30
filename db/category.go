d4c4a821ba7be48a39d661703d8f6a58e53cce66
Ranel <febranel@gmail.com>
2024-09-30 08:38:16 -0400

    feat: storage category color in db

--- db/category.go
+++ db/category.go
@@ -36,13 +36,13 @@ func UpdateCategoryInDb(newCategory models.Category) error {
 
 func GetCategoryInDb(id int) (models.Category, error) {
 	sqlScript := `
-		SELECT id, name, description
+		SELECT id, name, description, color
 		FROM categories
 		WHERE id= $1
 	`
 	var category models.Category
 	row := Pool.QueryRow(context.Background(), sqlScript, id)
-	err := row.Scan(&category.Id, &category.Name, &category.Description)
+	err := row.Scan(&category.Id, &category.Name, &category.Description, &category.Color)
 	if err != nil {
 		return models.Category{}, err
 	}
@@ -72,16 +72,18 @@ func GetCategoryName(id int) (string, error) {
 }
 
 func GetAllCategoriesFromDb() ([]struct {
-	Name string `json:"name"`
-	Id   int    `json:"id"`
+	Name  string `json:"name"`
+	Color string `json:"color"`
+	Id    int    `json:"id"`
 }, error) {
 	sqlScript := `
-		SELECT id, name
+		SELECT id, name, color
 		FROM categories
 	`
 	var result []struct {
-		Name string `json:"name"`
-		Id   int    `json:"id"`
+		Name  string `json:"name"`
+		Color string `json:"color"`
+		Id    int    `json:"id"`
 	}
 	rows, err := Pool.Query(context.Background(), sqlScript)
 	if err != nil {
@@ -92,16 +94,19 @@ func GetAllCategoriesFromDb() ([]struct {
 	for rows.Next() {
 		var name string
 		var id int
-		err := rows.Scan(&id, &name)
+		var color string
+		err := rows.Scan(&id, &name, &color)
 		if err != nil {
 			return nil, err
 		}
 		result = append(result, struct {
-			Name string `json:"name"`
-			Id   int    `json:"id"`
+			Name  string `json:"name"`
+			Color string `json:"color"`
+			Id    int    `json:"id"`
 		}{
-			Name: name,
-			Id:   id,
+			Name:  name,
+			Color: color,
+			Id:    id,
 		})
 	}
 	return result, nil
@@ -109,10 +114,11 @@ func GetAllCategoriesFromDb() ([]struct {
 
 func GetAllCategoriesWithTotals() ([]struct {
 	Name  string  `json:"name"`
+	Color string  `json:"color"`
 	Total float32 `json:"total"`
 }, error) {
 	sqlScript := `
-		select c.name, sum(e.amount) as total
+		select c.name, sum(e.amount) as total, c.color
 		from categories c 
 		join expenses e 
 		on c.id = e.category_id 
@@ -120,6 +126,7 @@ func GetAllCategoriesWithTotals() ([]struct {
 	`
 	var result []struct {
 		Name  string  `json:"name"`
+		Color string  `json:"color"`
 		Total float32 `json:"total"`
 	}
 	rows, err := Pool.Query(context.Background(), sqlScript)
@@ -130,15 +137,18 @@ func GetAllCategoriesWithTotals() ([]struct {
 	for rows.Next() {
 		var name string
 		var total float32
-		err := rows.Scan(&name, &total)
+		var color string
+		err := rows.Scan(&name, &total, &color)
 		if err != nil {
 			return nil, err
 		}
 		result = append(result, struct {
 			Name  string  `json:"name"`
+			Color string  `json:"color"`
 			Total float32 `json:"total"`
 		}{
 			Name:  name,
+			Color: color,
 			Total: total,
 		})
 	}
--- models/models.go
+++ models/models.go
@@ -29,6 +29,7 @@ type Category struct {
 	Id          int       `json:"id"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
+	Color       string    `json:"color"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
