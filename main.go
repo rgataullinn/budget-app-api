2436ef4801e48b965f313403167d61708025fdb6
Ranel <febranel@gmail.com>
2025-03-01 02:36:28 -0500

    chore: add fe prod in to allowOrigins

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://bapp-production.up.railway.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length", "Set-Cookie"},
